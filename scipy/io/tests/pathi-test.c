183eeb99d82b20ebac847b3b5dde9473a91f167a
Sylwia Budzynska <102833689+sylwia-budzynska@users.noreply.github.com>
2026-04-10 23:37:40 +0200

    Document security issues in pathi-test.c
    Added comments indicating potential security issues with path injection and overflow checks.

--- scipy/io/tests/pathi-test.c
+++ scipy/io/tests/pathi-test.c
@@ -4,7 +4,12 @@ int main(int argc, char** argv) {
   {
     char fileBuffer[PATH_MAX];
     snprintf(fileBuffer, sizeof(fileBuffer), "/home/%s", userAndFile);
-    // BAD: a string from the user is used in a filename
+    // BAD: a string from the user is used in a filename extended and security-and-quality https://codeql.github.com/codeql-query-help/cpp/cpp-path-injection/
     fopen(fileBuffer, "wb+");
   }
+}
+
+bool checkOverflow(unsigned short x, unsigned short y) {
+  // BAD: comparison is always false due to type promotion https://codeql.github.com/codeql-query-help/cpp/cpp-bad-addition-overflow-check/
+  return (x + y < x);  
 }
