3f16813c81a6f14a5aeeea41320a85305e6b12fc
Sylwia Budzynska <102833689+sylwia-budzynska@users.noreply.github.com>
2026-05-07 16:45:49 +0200

    vuln for testing default query suite

--- scipy/_lib/tests/test_boost_build.cpp
+++ scipy/_lib/tests/test_boost_build.cpp
@@ -6,3 +6,12 @@
 void test() {
   boost::math::binomial_distribution<double> d(10, 0.5);
 }
+
+extern bool checkOverflow(unsigned short x, unsigned short y) {
+  // BAD: comparison is always false due to type promotion https://codeql.github.com/codeql-query-help/cpp/cpp-bad-addition-overflow-check/
+  return (x + y < x);  
+}
+
+int x = atoi(argv[1]);
+int y = atoi(argv[2]);
+checkOverflow(x,y);
